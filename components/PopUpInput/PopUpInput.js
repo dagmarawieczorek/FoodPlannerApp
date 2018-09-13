f63265a50672838dfdaf4a6ecbc261131b727947
Dagmara Wieczorek <dagmara.sylwia.w@gmail.com>
2018-09-13 18:00:03 +0200

    inputs in modal, lists add items to realm using categories database

---
+++ components/PopUpInput/PopUpInput.js
@@ -0,0 +1,141 @@
+import React, {Component} from 'react';
+import {Button, Picker, Text, TextInput, View} from 'react-native';
+import styles from "../../styles/styles.js";
+import realm from "../../databases/allSchemas";
+import data from "../../databases/categories";
+
+class PopUpInput extends Component {
+
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: data,
+            subCategory: data[0].items,
+            newItemName: "",
+            pickedCat: data[0].title,
+            pickedSubcat: data[0].items[0],
+        };
+
+    }
+
+
+    handleNewText(text) {
+        this.setState({
+            value: text
+        })
+    }
+
+
+    handleSubcategoryChange = (value) => {
+        this.setState({
+            pickedSubcat: value,
+        });
+    };
+
+    handleCategoryChange = (value) => {
+
+        const foundSubcategory = this.state.data.find(category => {
+            return category.title === value;
+        });
+
+        this.setState({
+            pickedCat: value,
+            subCategory: foundSubcategory.items,
+        });
+    };
+
+
+    handleButtonNewItem = () => {
+
+
+
+        if (this.state.newItemName !== "" && this.state.newItemPrice!== 0) {
+
+            let newId = realm.objects("Ingredient").length + 1;
+
+            let newProduct = {
+                id: newId,
+                name: this.state.newItemName,
+                price: Number(this.state.newItemPrice),
+                category: this.state.pickedCat,
+                subcategory: this.state.pickedSubcat
+            };
+
+            realm.write(() => {
+                realm.create("Ingredient", newProduct);
+                console.log("Saving " + newProduct.name);
+            });
+        }
+
+
+      this.props.onClose();
+    };
+
+    render() {
+
+        const lisCategory = this.state.data.map(item => (
+            <Picker.Item label={item.title} key={item.title} value={item.title}/>
+        ));
+
+
+        const lisSubcategory = this.state.subCategory.map(item => (
+            <Picker.Item label={item} key={item} value={item}/>
+        ));
+
+        console.log(realm.objects("Ingredient").length + 1);
+        return (<View style={styles.popUpInputBox}>
+
+            <Text>Wpisz nazwę | Cene</Text>
+            <View style={styles.popUpInputs}>
+
+
+                <TextInput
+                    style={{height: 40, width: 200, borderColor: 'gray', borderWidth: 1}}
+                    onChangeText={text => this.setState({newItemName: text})}
+                    value={this.state.newItemName}
+                />
+
+                <TextInput
+                    style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                    keyboardType="numeric"
+                    onChangeText={text => this.setState({newItemPrice: text})}
+                    value={this.state.newItemPrice}
+                />
+
+            </View>
+
+
+            <Text>Wybierz kategorie</Text>
+            {/*/pick category*/}
+            <Picker
+                selectedValue={this.state.pickedCat}
+                style={{height: 50, width: 300}}
+                mode="dropdown"
+                onValueChange={this.handleCategoryChange}>
+                {lisCategory}
+            </Picker>
+
+
+            {/*/pick subcategory*/}
+            <Picker
+                selectedValue={this.state.pickedSubcat}
+                style={{height: 50, width: 300}}
+                mode="dropdown"
+                onValueChange={this.handleSubcategoryChange}>
+                {lisSubcategory}
+            </Picker>
+
+            <Button
+                onPress={() => this.handleButtonNewItem()}
+                title="+"
+                color="gray"
+                accessibilityLabel="add"
+            />
+
+        </View>)
+    }
+}
+
+export default PopUpInput;
---
+++ databases/categories.js
@@ -0,0 +1,74 @@
+let categories=[
+    {
+        title: 'groceries',
+        items: ["Salt and sugar",
+            "Flour",
+            "Pasta, rice and groats",
+            "Breakfast cereals and muesli",
+            "Oil, olive oil and vinegar",
+            "Spices",
+            "Sauces",
+            "Canned",
+            "Ready meals and soups",
+            "Fruit preserves, honey",
+            "Baking products",
+            "Sweets",
+            "Salty snacks",
+            "Cuisines of the world",
+            "Healthy food",],
+    },
+    {
+        title: 'dairy',
+        items: ["Milk",
+            "Yoghurt",
+            "Sour cream",
+            "Milk desserts",
+            "Cheese",
+            "Butter and margarine",
+            "Eggs and yeast",
+            "Healthy food"],
+    },
+
+    {
+        title: 'fruits and vegetables',
+        items: ["Fruits",
+            "Vegetables",
+            "Salads",
+            "Herbs",
+            "Mushrooms",
+            "Nuts and grainy",
+            "Dried fruits and nuts and dried fruits",]
+    },
+
+    {
+        title: "Pastry bakery",
+        items: ["Bread",
+            "Packaged bread",
+            "Rolls",
+            "Salty snacks",
+            "Sweet pastries",
+            "Cakes and desserts",
+            "Toast and bread for baking",
+            "Gluten-free bread",
+        ]
+    },
+
+    {
+        title: "Drinks",
+
+        items: [
+            "Tea",
+            "Coffee",
+            "Cocoa, hot chocolate and cappucino",
+            "Water",
+            "Fresh juice",
+            "Juices, nectars, fruit drinks",
+            "Carbonated drinks",
+            "Ice Tea",
+            "Syrups",
+            "Energy and isotonic drinks",
+            "Alkohol free drinks",]
+    }
+];
+
+export default categories;
